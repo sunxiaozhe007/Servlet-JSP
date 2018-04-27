5cf398ba3e66ce2ad9f91f59f5ef2a7498bd8a0b
sunxiaozhe007 <985160269@qq.com>
2018-04-27 22:08:46 +0800

    DBUtil实例

---
+++ Demo_17/src/com/itheima/util/Test.java
@@ -0,0 +1,20 @@
+package com.itheima.util;
+
+import java.sql.SQLException;
+
+public class Test {
+
+    public static void main(String[] args) throws SQLException {
+            TestRUD s = new TestRUD();
+            //s.testSelect();
+            //s.testInsert();
+        //s.testUpdate();
+        //s.testDelete();
+        TestResultSetHandler s1 = new TestResultSetHandler();
+        //s1.test1();
+        //s1.test2();
+        //s1.test3();
+        //s1.test4();
+        s1.test5();
+    }
+}

8c3fad9cc3fd158f15cd7ed6b8c8f227d18b6b98
sunxiaozhe007 <985160269@qq.com>
2018-04-30 22:22:42 +0800

    删除

---
+++ Students/src/servlet/StudentDelServlet.java
@@ -0,0 +1,25 @@
+package servlet;
+
+import service.StudentServiceImpl;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "servlet.StudentDelServlet", urlPatterns = {"/StudentDelServlet"})
+public class StudentDelServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String id = request.getParameter("id");
+        StudentServiceImpl studentService = new StudentServiceImpl();
+        studentService.deleteStudent(id);
+        request.getRequestDispatcher("/list").forward(request,response);
+
+    }
+}
